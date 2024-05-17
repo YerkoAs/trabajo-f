738402b361a6d68bae5f7c1fd8ff3f53b3616730
Yerk Asbun <asbunyerko01@gmail.com>
2024-05-17 06:52:41 -0400

    portafolio final

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portafolio personal hecho con HTML CSS y JavaScript
+Este portafolio fue hecho a modo de practica y examen final en el modulo de fundamentos del desarrollo web, generacion 38.
+## Partes del Portafolio
+- Inicio
+- A cerca de mi
+- Habilidades
+- Mis trabajos
+- Contacto
+## Tecnologias utilizadas en este portafolio
+Como se menciono a un principio se utilizo lo siguiente:
+- HTML para la estructura
+- CSS para los estilos
+- JavaScript para la funcionalidad
+- Git para crear un repositorio tanto local como remoto
--- index.js
+++ index.js
@@ -1,42 +1,8 @@
-/* import Typed from 'typed.js';
-
-const Typed = new Typed('#typing', {
-  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-  typeSpeed: 10,
-}); */
-
-
-
-
-/* const typed = new Typed('#typing', {
-    strings: ['Photographer', 'Developer', 'Designer'],
-    typeSpeed: 50,
-    backSpeed : 50,
-    startDelay : 25,
-    loop : true,
-    loopCount : Infinity
-  }); */
- 
- 
- 
- /* function openMenu(){
-    menu.classList.add("display-menu")
-    } */
-    /* function closeMenu(){
-    menu.classList.remove("display-menu")
-    } */
-    /* arc.onclick = function(){
-        alert("hola")
-    } */
-    //openButton.onclick = openMenu
-    //openButton.addEventListener('click', openMenu)
 
     const nav = document.querySelector('nav')
     const listas = document.querySelectorAll('#menu-list-item')
     nav.addEventListener('click', function (pipoca) {
-        /* if (event.target.closest('#openButton')) {
-            menu.classList.add("display-menu")   
-           } */
+        
 
         if (pipoca.target.closest('#openButton')) {
             menu.classList.add("display-menu")   
@@ -55,43 +21,11 @@ const Typed = new Typed('#typing', {
         }
         
     }) 
+
+    
     
 
 
-    
-
-    /* openButton.addEventListener('click', openMenu)
-    let ar = document.getElementById("arc")
-    //ar.innerText = "clickea"
-    let h1 = document.querySelector("h1")
-    //h1.innerText = "Goodbye" 
-
-const lista1 = document.querySelectorAll('.lista li')
-console.log(lista1)
-const lista2 = Array.from(lista1)
-console.log(lista2)
-for (const li of lista2) {
-    console.log(li)
-    li.style.backgroundColor = "lightgreen"
-}
-//className reemplaza todas las clases
-const caja = document.querySelector(".box")
-//caja.className = "caja"
-//classList adiciona o remueve una clase 
-caja.classList.add('caja1', 'caja2')
-caja.classList.remove('caja1')
-
-const cajita = document.querySelector('#box')
-const boton = document.querySelector('#box-btn')
-boton.onclick = function () {
-    cajita.classList.toggle('hiden')
-    
-} */
-//background-size : cover
-
-//object-fit : cover  ---> siempre que se meta la imagen en un div
-
-//<script src="https://unpkg.com/scrollreveal"></script>
 const sr = new ScrollReveal({
     origin :'top',
     distance : '80px',
@@ -99,6 +33,14 @@ const sr = new ScrollReveal({
     reset : true
 })
 
-//sr.reveal('.hero-tittle', { origin :'top',distance : '80px' });
-//absolute  relative
+
+  var typed = new Typed('#typing', {
+    strings: ['Photographer', 'Developer', 'Designer'],
+    typeSpeed: 50,
+    backSpeed : 50,
+    startDelay : 25,
+    loop : true,
+    loopCount : Infinity
+});
+
 
